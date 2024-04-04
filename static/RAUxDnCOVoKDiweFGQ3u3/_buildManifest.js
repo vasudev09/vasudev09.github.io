2bb9345511cf7d9e17039590c7a18828fcf88129
vasudev09 <vasudevaraju2525@gmail.com>
2024-04-04 14:16:59 +0530

    initial build commit

---
+++ static/RAUxDnCOVoKDiweFGQ3u3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/6b25432f67ef1376.css","static/chunks/pages/index-cfb0b8d1f07619bd.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
